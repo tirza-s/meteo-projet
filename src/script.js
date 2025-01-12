f26592044d7bc7a395cff6b129a74785ade21ef8
Tirza Samosir <tirzasamosir@MacBook-Air-de-Tirza.local>
2025-01-12 17:15:35 +0100

    add humidity and wind

--- src/script.js
+++ src/script.js
@@ -1,15 +1,27 @@
-function updateCurrentWeather(response) {
-    let temperatureValueElement = document.getElementById("current-temperature-value");
-    let temperature = Math.round(response.data.temperature.current)
-    temperatureValueElement.textContent = temperature;
-}
-
 function cityScanning(city) {
     // make api call and update the interface
     let apiKey = "b164e02a4a760t33f3o317f1aa0024b6";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(updateCurrentWeather);
 }
+function updateCurrentWeather(response) {
+    let temperatureValueElement = document.getElementById("current-temperature-value");
+    let temperature = Math.round(response.data.temperature.current)
+    temperatureValueElement.textContent = temperature;
+
+    let descriptionElemement = document.getElementById("description");
+    let description = response.data.condition.description;
+    descriptionElemement.textContent = description;
+
+    let humidityElement = document.getElementById("humidity-details");
+    let currentHumidity = Math.round(response.data.temperature.humidity);
+    humidityElement.innerHTML = `Humidity : <strong>${currentHumidity}%</strong>`;
+
+    let windElement = document.getElementById("wind-speed");
+    let currentWind = Math.round(response.data.wind.speed);
+    windElement.innerHTML = `Wind : <strong>${currentWind} </strong>`
+}
+
 
 function displayCurrentCity(event) {
     event.preventDefault();
@@ -27,9 +39,8 @@ function displayCurrentCity(event) {
     cityScanning(formattedCity)
 }
 
-
 let searchForm = document.getElementById("search-form");
 searchForm.addEventListener("submit", displayCurrentCity);
 
 
-displayCurrentCity("Brussels");
+cityScanning("Brussels");
